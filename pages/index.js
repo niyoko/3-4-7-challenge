74ec4bb99917fa855e403e7f6f2835abc98e1e44
Niyoko Yuliawan <niyoko.yuliawan@logisly.com>
2022-01-02 01:11:03 +0700

    Complete

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,53 @@ import {
   Container,
   Box,
   Stack,
+  Grid,
   Typography,
   Slider,
   Checkbox,
   FormControlLabel,
+  FormLabel,
   FormGroup,
+  IconButton,
 } from "@mui/material";
+import { PlayCircle, PauseCircle } from "@mui/icons-material";
 import { some } from "lodash-es";
 import dynamic from "next/dynamic";
 import gcd from "compute-gcd";
 import { useDebounce } from "react-use";
 const Canvas = dynamic(() => import("../src/MainCanvas"), { ssr: false });
 
-const MAX = 50;
+const MAX = 30;
 const MIN = 3;
+
+const gonNames = [
+  "",
+  "monogon",
+  "line",
+  "triangle",
+  "square",
+  "pentagon",
+  "hexagon",
+  "heptagon",
+  "octagon",
+  "nonagon",
+  "decagon",
+  "hendecagon",
+  "dodecagon",
+  "tridecagon",
+  "tetradecagon",
+  "pentadecagon",
+  "hexadecagon",
+  "heptadecagon",
+  "octadecagon",
+  "enneadecagon",
+  "icosagon",
+];
+
+const getGonName = (n) => {
+  if (gonNames[n]) return gonNames[n] + "s";
+  return `${n}-gons`;
+};
 
 const isCoprime = (a, b) => gcd(a, b) === 1;
 const hasCoprime = (a) => {
@@ -27,18 +60,9 @@ const hasCoprime = (a) => {
   return false;
 };
 
-const getAMarks = () => {
-  const marks = [];
-  for (let i = MIN; i <= MAX; i++) {
-    if (hasCoprime(i)) marks.push({ value: i });
-  }
-
-  return marks;
-};
-
 const getMarks = (a) => {
   const marks = [];
-  for (let i = 2; i <= a; i++) {
+  for (let i = 1; i <= a; i++) {
     if (isCoprime(a, i)) {
       marks.push({ value: i });
     }
@@ -51,14 +75,41 @@ const useIsomorphicLayoutEffect =
 
 export default function Index() {
   const [showStar, setShowStar] = React.useState(true);
+  const [showPoints, setShowPoints] = React.useState(true);
+  const [showG1, setShowG1] = React.useState(false);
+  const [showG2, setShowG2] = React.useState(false);
+
+  const [isStopped, setIsStopped] = React.useState(false);
+  const [animationSpeed, setAnimationSpeed] = React.useState(10);
+  const [starRoundness, setStarRoundness] = React.useState(10);
   const [a, setA] = React.useState(7);
   const [b, setB] = React.useState(3);
   const [props, setProps] = React.useState();
 
-  const aMarks = React.useMemo(() => getAMarks(), []);
   const bMarks = React.useMemo(() => getMarks(a), [a]);
 
-  useDebounce(() => setProps({ a, b, showStar }), 250, [a, b, showStar]);
+  React.useEffect(() => {
+    setProps({
+      a,
+      b,
+      showStar,
+      showPoints,
+      showG1,
+      showG2,
+      animationSpeed: isStopped ? 0 : animationSpeed * 0.001,
+      starRoundness: starRoundness,
+    });
+  }, [
+    a,
+    b,
+    showStar,
+    showPoints,
+    showG1,
+    showG2,
+    animationSpeed,
+    isStopped,
+    starRoundness,
+  ]);
 
   useIsomorphicLayoutEffect(() => {
     if (!some(bMarks, (bm) => bm.value === b)) {
@@ -66,31 +117,64 @@ export default function Index() {
     }
   }, [bMarks, b]);
 
-  const setASlider = React.useCallback((e, value) => setA(value), []);
-  const setBSlider = React.useCallback((e, value) => setB(value), []);
-
   const isNumberCoprime = React.useMemo(() => isCoprime(a, b), [a, b]);
 
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="lg">
       <Box sx={{ my: 4 }}>
-        <Stack spacing={2}>
-          <Slider
-            step={null}
-            marks={aMarks}
-            value={a}
-            min={3}
-            max={MAX}
-            onChange={setASlider}
-          />
-          <Slider
-            step={null}
-            marks={bMarks}
-            value={b}
-            min={2}
-            max={MAX}
-            onChange={setBSlider}
-          />
+        <Stack spacing={3}>
+          <Grid container columnSpacing={5} rowSpacing={3}>
+            <Grid item xs={12} md={6}>
+              <Stack>
+                <FormLabel>First number: {a}</FormLabel>
+                <Slider
+                  step={1}
+                  marks
+                  value={a}
+                  min={2}
+                  max={MAX}
+                  onChange={(e, value) => setA(value)}
+                />
+              </Stack>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Stack>
+                <FormLabel>Second number: {b}</FormLabel>
+                <Slider
+                  step={null}
+                  marks={bMarks}
+                  value={b}
+                  min={1}
+                  max={MAX}
+                  onChange={(e, value) => setB(value)}
+                />
+              </Stack>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Stack>
+                <FormLabel>Animation speed: {animationSpeed}</FormLabel>
+                <Stack direction="row" alignItems="center" spacing={1}>
+                  <Slider
+                    value={animationSpeed}
+                    onChange={(e, value) => setAnimationSpeed(value)}
+                  />
+                  <IconButton onClick={() => setIsStopped((x) => !x)}>
+                    {isStopped ? <PlayCircle /> : <PauseCircle />}
+                  </IconButton>
+                </Stack>
+              </Stack>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Stack>
+                <FormLabel>Star roundness: {starRoundness}</FormLabel>
+                <Slider
+                  value={starRoundness}
+                  onChange={(e, value) => setStarRoundness(value)}
+                />
+              </Stack>
+            </Grid>
+          </Grid>
+
           <FormGroup row>
             <FormControlLabel
               control={
@@ -101,13 +185,46 @@ export default function Index() {
               }
               label="Show star"
             />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={showPoints}
+                  onChange={(e) => setShowPoints(e.target.checked)}
+                />
+              }
+              label="Show points"
+            />
+            {b > 1 && (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={showG1}
+                    onChange={(e) => setShowG1(e.target.checked)}
+                  />
+                }
+                label={`Show ${getGonName(b)}`}
+              />
+            )}
+            {a - b > 1 && (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={showG2}
+                    onChange={(e) => setShowG2(e.target.checked)}
+                  />
+                }
+                label={`Show ${getGonName(a - b)}`}
+              />
+            )}
           </FormGroup>
           <Typography
             color={isCoprime ? undefined : "error"}
-          >{`Label: {${a}/${b}} ${
+          >{`Star label: {${a}/${b}} ${
             isNumberCoprime ? "" : "Pair is not coprime"
           }`}</Typography>
-          <Canvas {...props} />
+          <Stack justifyContent="center" alignItems="center">
+            <Canvas {...props} />
+          </Stack>
         </Stack>
       </Box>
     </Container>
--- src/MainCanvas.js
+++ src/MainCanvas.js
@@ -3,9 +3,45 @@ import * as PIXI from "pixi.js";
 
 const SIZE = 640;
 const s = 250;
-const speed = 0.02;
 
-function setPos(alpha, r1, r2, pos) {
+/**
+ * @param {PIXI.Point} p1
+ * @param {PIXI.Point} p2
+ * */
+function dist(p1, p2) {
+  const xg = p1.x - p2.x;
+  const yg = p1.y - p2.y;
+  return Math.sqrt(xg * xg + yg * yg);
+}
+
+/** @param {PIXI.Point[]} points */
+function reorderPoints(points, cache) {
+  if (points.length <= 3) return points;
+
+  if (!cache.value) {
+    const pref = points[0];
+    let min = Number.MAX_VALUE;
+    for (let i = 1; i < points.length; i++) {
+      const d = dist(pref, points[i]);
+      if (d < min) {
+        min = d;
+        cache.value = i;
+      }
+    }
+  }
+
+  const g = cache.value;
+  const np = [];
+  let j = 0;
+  while (np.length < points.length) {
+    np.push(points[j]);
+    j = (j + g) % points.length;
+  }
+
+  return np;
+}
+
+function calculatePos(alpha, r1, r2) {
   const p1x = r1 * Math.cos(alpha);
   const p1y = r1 * Math.sin(alpha);
 
@@ -14,94 +50,173 @@ function setPos(alpha, r1, r2, pos) {
   const p2x = p1x + r2 * Math.cos(gamma);
   const p2y = p1y - r2 * Math.sin(gamma);
 
-  pos.set(s * p2x + SIZE / 2, SIZE / 2 - s * p2y);
+  return [s * p2x + SIZE / 2, SIZE / 2 - s * p2y];
 }
 
-const makePoint = (app, points) => {
-  const point = new PIXI.Graphics();
-  point.beginFill(0xffffff);
-  point.drawCircle(0, 0, 3);
-  point.endFill();
-  points.push(point);
-  app.stage.addChild(point);
-};
-
 const makeApp = (canvas, params) => {
-  const app = new PIXI.Application({ antialias: true, view: canvas });
-
-  const brush = new PIXI.Graphics();
-  brush.beginFill(0xff0000);
-  brush.drawRect(0, 0, 2, 2);
-  brush.endFill();
-
-  const blackBrush = new PIXI.Graphics();
-  blackBrush.beginFill(0x000000);
-  blackBrush.drawRect(0, 0, 2, 2);
-  blackBrush.endFill();
-
-  const renderTexture = PIXI.RenderTexture.create({
+  const app = new PIXI.Application({
+    antialias: true,
+    autoDensity: true,
+    view: canvas,
     width: SIZE,
     height: SIZE,
+    backgroundColor: 0xffffff,
   });
 
-  const renderTextureSprite = new PIXI.Sprite(renderTexture);
-  renderTextureSprite.filters = [new PIXI.filters.FXAAFilter()];
-  app.stage.addChild(renderTextureSprite);
-  const opt = { clear: false, renderTexture };
+  const starContainer = new PIXI.Container();
+  starContainer.zIndex = 0;
 
-  const points = [];
-  for (let i = 0; i < 50; i++) {}
+  const g2Container = new PIXI.Container();
+  g2Container.zIndex = 2;
+
+  const g1Container = new PIXI.Container();
+  g1Container.zIndex = 3;
+
+  const pointsContainer = new PIXI.Container();
+  pointsContainer.zIndex = 4;
+
+  const star = new PIXI.Graphics();
+  starContainer.addChild(star);
 
   let phase = 0;
   const prev = {};
 
+  const setMode = (name, container) => {
+    if (prev[name] !== params[name]) {
+      prev[name] = params[name];
+      if (params[name]) app.stage.addChildAt(container);
+      else app.stage.removeChild(container);
+      app.stage.sortChildren();
+    }
+  };
+
   app.ticker.add((delta) => {
-    const { a, b, showStar } = params;
+    const { a, b, animationSpeed, starRoundness } = params;
     const wrap = 2 * b * Math.PI;
-    phase += speed * delta;
-    phase = phase % wrap;
-    const r1 = (a - b) / a;
-    const r2 = 1 - r1 - 0.05;
 
     const lim = b * (a - b);
-    for (let i = 0; i < Math.max(points.length, lim); i++) {
-      if (i >= points.length) makePoint(app, points);
-      if (i < lim) {
-        setPos(phase + i * (wrap / lim), r1, r2, points[i].position);
-      } else points[i].position.set(-5, -5);
+
+    phase += animationSpeed * delta;
+    phase = phase % wrap;
+    const r1 = (a - b) / a;
+    const roundnessRange = 1 - r1;
+    const r2 = 1 - r1 - (starRoundness / 100) * roundnessRange;
+
+    if (prev.a !== a || prev.b !== b) {
+      prev.gg1 = {};
+      prev.gg2 = {};
     }
 
-    if (prev.a !== a || prev.b !== b || prev.showStar !== showStar) {
+    if (prev.a !== a || prev.b !== b || prev.starRoundness !== starRoundness) {
       prev.a = a;
       prev.b = b;
-      prev.showStar = showStar;
+      prev.starRoundness = starRoundness;
 
-      const sens = 0.003;
-      const tot = showStar ? Math.ceil(wrap / sens) : 0;
+      const sens = 0.01;
+      const tot = Math.ceil(wrap / sens);
 
+      star.clear();
+      star.lineStyle(2, 0x747374);
       for (let i = 0; i <= tot; i++) {
-        setPos(i * sens, r1, r2, brush.position);
-        if (i === 0) app.renderer.render(blackBrush, { ...opt, clear: true });
-        else app.renderer.render(brush, opt);
+        const pos = calculatePos(i * sens, r1, r2);
+        if (i === 0) star.moveTo(...pos);
+        else star.lineTo(...pos);
       }
+
+      pointsContainer.removeChildren();
+      for (let i = 0; i < lim; i++) {
+        const cont = new PIXI.Container();
+        const point = new PIXI.Graphics();
+
+        point.beginFill(0x556cd6);
+        point.drawCircle(0, 0, 5);
+        point.endFill();
+
+        cont.addChild(point);
+        pointsContainer.addChild(cont);
+      }
+
+      g1Container.removeChildren();
+      g2Container.removeChildren();
+      for (let i = 0; i < a - b; i++) {
+        g1Container.addChild(new PIXI.Graphics());
+      }
+
+      for (let i = 0; i < b; i++) {
+        g2Container.addChild(new PIXI.Graphics());
+      }
+    }
+
+    setMode("showStar", starContainer);
+    setMode("showPoints", pointsContainer);
+    setMode("showG1", g1Container);
+    setMode("showG2", g2Container);
+
+    const len = pointsContainer.children.length;
+    for (let i = 0; i < len; i++) {
+      const point = pointsContainer.getChildAt(i);
+      point.position.set(...calculatePos(phase + i * (wrap / lim), r1, r2));
+    }
+
+    for (let i = 0; i < a - b; i++) {
+      const gp = (k) => (i + 1 * (a - b) * k) % lim;
+
+      const points = [];
+      for (let j = 0; j < b; j++) {
+        points.push(pointsContainer.getChildAt(gp(j)).position);
+      }
+
+      /** @type {PIXI.Graphics} */
+      const g = g1Container.getChildAt(i);
+      g.clear();
+      g.lineStyle(2, 0x00ff00);
+      g.drawPolygon(reorderPoints(points, prev.gg1));
+    }
+
+    for (let i = 0; i < b; i++) {
+      const gp = (k) => (i + b * k) % lim;
+
+      const points = [];
+      for (let j = 0; j < a - b; j++) {
+        points.push(pointsContainer.getChildAt(gp(j)).position);
+      }
+
+      /** @type {PIXI.Graphics} */
+      const g = g2Container.getChildAt(i);
+      g.clear();
+      g.lineStyle(2, 0xff6666);
+      g.drawPolygon(reorderPoints(points, prev.gg2));
     }
   });
 
   return app;
 };
 
-export default function ({ a, b, showStar }) {
-  /** @type {{current: {app: PIXI.Application}}} */
-  const ref = React.useRef({});
-  const params = React.useRef({ a, b, showStar });
+export default function ({
+  a,
+  b,
+  showStar,
+  showPoints,
+  showG1,
+  showG2,
+  animationSpeed,
+  starRoundness,
+}) {
+  const params = React.useRef({
+    a,
+    b,
+    showStar,
+    showPoints,
+    showG1,
+    animationSpeed,
+    starRoundness,
+  });
   const [canvas, setCanvas] = React.useState();
 
   React.useEffect(() => {
     if (canvas) {
       const app = makeApp(canvas, params.current);
-      ref.current.app = app;
       return () => {
-        ref.current = {};
         app.destroy();
       };
     }
@@ -111,7 +226,21 @@ export default function ({ a, b, showStar }) {
     params.current.a = a;
     params.current.b = b;
     params.current.showStar = showStar;
-  }, [a, b, showStar]);
+    params.current.showPoints = showPoints;
+    params.current.showG1 = showG1;
+    params.current.showG2 = showG2;
+    params.current.animationSpeed = animationSpeed;
+    params.current.starRoundness = starRoundness;
+  }, [
+    a,
+    b,
+    showStar,
+    showPoints,
+    showG1,
+    showG2,
+    animationSpeed,
+    starRoundness,
+  ]);
 
   return <canvas ref={setCanvas} width={SIZE} height={SIZE} />;
 }
